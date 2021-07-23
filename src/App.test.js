a0f57c500e2f7b4a10a9743fadc8d171bc0c5c5e
Basil <basel_marwan@icloud.com>
2021-07-23 11:00:14 +0200

    fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders IP Address locator link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/IP Address locator/i);
   expect(linkElement).toBeInTheDocument();
 });
