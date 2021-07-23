f39a1b1da38ead51b0050dafdae89d80d0b8c367
Basil <basel_marwan@icloud.com>
2021-07-23 11:04:33 +0200

    fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders IP Address locator link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/IP Address locator/i);
+  const linkElement = screen.getByText(/IP Address/i);
   expect(linkElement).toBeInTheDocument();
 });
