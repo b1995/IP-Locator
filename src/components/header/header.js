47b898ace426a0169758abaaa1f4f35c0b3bf6db
Basil <basel_marwan@icloud.com>
2021-07-24 08:56:21 +0200

    fixed search bar and header text

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,19 @@
 
 import background from '../../Assets/pattern-bg.png'
+import '../../App.css'
 function Headertext() {
     return (
 
-        <header className="App-header" style = {{backgroundImage: `url(${background})` }}>
+        <header className="App-header">
+          <img src = {background} className = "Header-img" alt = "log" />
+          <h1 className = "Header-text">IP Address Tracker</h1>
+          <form action = "/" method = 'get' className = 'Search-bar'>
+            <input
+            type = "text"
+            />
+            <button type="submit"><i class="arrow right"></i></button>
+          </form>
 
-          <h1>IP Address Tracker</h1>
         </header>
     );
   }
