76d4dd4a377d61d54769032898893ebc618bea5c
Basil <basel_marwan@icloud.com>
2021-07-25 22:09:36 +0200

    Created Map components and pin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import pattern from './pattern-bg.png';
 import './App.css';
+import Mapsection from './components/Maps/Map.js'
+
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+} 
+
 
 function App() {
   return (
@@ -8,6 +16,9 @@ function App() {
         <img src={ pattern } alt="logo" />
         <div>IP Address Tracker</div>
       </header>
+      <body>
+        <Mapsection location = { location } zoomLevel = { 17 }/>
+      </body>
     </div>
   );
 }
