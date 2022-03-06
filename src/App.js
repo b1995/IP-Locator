62bce46527c74d9fc0accb969fcc6b6bc21cbc97
basil <bawwad032@gmail.com>
2022-03-06 21:06:08 +0300

    slight fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import pattern from './pattern-bg.png';
 import './App.css';
 
-/*const location = [
+const location = [
   address: '1600 Amphitheatre Parkway, Mountain View, california.',
   lat: 37.42216,
   lng: -122.08427,
-]*/
+]
 
 
 function App() {
